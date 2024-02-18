643c752298fc27cc84ec83bcc7490409f04a2881
balindam <balindam.c@gmail.com>
2024-02-18 15:22:36 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tracking-id
+
+Upwork gig for a client. Lambda to track IP of the user, save in DB and redirect to a URL. DB is also on AWS, chosen AWS Aurora serverless V2 so that it can autoscale. Also used API gateway for exposing the endpoints which were attached to Lambda. Could have used Lambda URL as well but then we won't be able to extract the IP of the user. API gateway context provide IP of the user consuming the endpoint.
+
+Major requirement was to handle peak traffic of 500 users per sec.
+
+
+![1708249851098](image/README/1708249851098.png)
--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const mysql = require('mysql');
 const crypto = require('crypto');
 const util = require('util');
 
-// Database connection
-const db = mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 100, // adjust this according to your needs
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-// Promisify the query function for use with async/await
-db.query = util.promisify(db.query);
+// Promisify the pool query function for use with async/await
+pool.query = util.promisify(pool.query);
 
 // Generate a 22 character hash
 function generateHash() {
@@ -38,18 +39,18 @@ exports.handler = async (event, context) => {
   try {
     // Check if IP already exists in the database
     const checkIpQuery = "SELECT * FROM `offer_process_nfl` WHERE `ip` = ?";
-    const ipExists = await db.query(checkIpQuery, [ip]);
+    const ipExists = await pool.query(checkIpQuery, [ip]);
 
     if (ipExists.length > 0) {
       // IP exists, update timestamp
       const updateQuery = "UPDATE `offer_process_nfl` SET `date` = NOW() WHERE `ip` = ?";
-      await db.query(updateQuery, [ip]);
+      await pool.query(updateQuery, [ip]);
       hash = ipExists[0].hash;
     } else {
       // IP does not exist, insert new record
       hash = generateHash();
       const insertQuery = "INSERT INTO `offer_process_nfl`(`hash`, `date`, `pubid`, `ip`, `status`) VALUES (?, NOW(), ?, ?, 0)";
-      await db.query(insertQuery, [hash, user, ip]);
+      await pool.query(insertQuery, [hash, user, ip]);
     }
 
     console.log('Values:', [hash, user, ip]);
@@ -59,10 +60,10 @@ exports.handler = async (event, context) => {
   }
   
   const redirect_links = [
-    `https://sportsnlivehd.com/live-football.php?lang=en&promo=stream&flow=trial?subid=${user}&t1=${hash}`,
-    `https://sportsnlive.net/live-football.php?lang=en&promo=stream&flow=trial?subid=${user}&t1=${hash}`,
-    `https://sportsnlivehd.org/live-football.php?lang=en&promo=stream&flow=trial?subid=${user}&t1=${hash}`,
-    `https://sportslivehd.co/live-football.php?lang=en&promo=stream&flow=trial?subid=${user}&t1=${hash}`
+    `https://sportsnlivehd.com/live-football.php?lang=en&promo=stream&flow=trial&subid=${user}&t1=${hash}`,
+    `https://sportsnlive.net/live-football.php?lang=en&promo=stream&flow=trial&subid=${user}&t1=${hash}`,
+    `https://sportsnlivehd.org/live-football.php?lang=en&promo=stream&flow=trial&subid=${user}&t1=${hash}`,
+    `https://sportslivehd.co/live-football.php?lang=en&promo=stream&flow=trial&subid=${user}&t1=${hash}`
   ];
   
   // Get the next redirect link and update lastIndex
